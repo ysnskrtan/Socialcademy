e07cedd3af613853b3cf6607deff6d406ebde5ab
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-12-13 12:54:18 +0300

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Yasin Şükrü Tan on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
