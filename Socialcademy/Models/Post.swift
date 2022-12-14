3b3b8638eeb8ffd3a3c1635853eb3bb7cab6bb44
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-12-14 21:00:40 +0300

    ContentView renamed to PostsList

---
+++ Socialcademy/Models/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  Socialcademy
+//
+//  Created by Yasin Şükrü Tan on 14.12.2022.
+//
+
+import Foundation
---
+++ Socialcademy/Views/PostRow.swift
@@ -0,0 +1,20 @@
+//
+//  PostRow.swift
+//  Socialcademy
+//
+//  Created by Yasin Şükrü Tan on 14.12.2022.
+//
+
+import SwiftUI
+
+struct PostRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostRow_Previews: PreviewProvider {
+    static var previews: some View {
+        PostRow()
+    }
+}
