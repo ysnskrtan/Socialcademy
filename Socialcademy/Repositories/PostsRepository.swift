e3d8ae9288e39e31569fe7a70d00784c35d4a831
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2023-01-06 11:15:12 +0300

    Fetch favourite method added

--- Socialcademy/Repositories/PostsRepository.swift
+++ Socialcademy/Repositories/PostsRepository.swift
@@ -12,7 +12,8 @@ import FirebaseFirestoreSwift
 // MARK: - PostsRepositoryProtocol
 
 protocol PostsRepositoryProtocol {
-    func fetchPosts() async throws -> [Post]
+    func fetchAllPosts() async throws -> [Post]
+    func fetchFavoritePosts() async throws -> [Post]
     func create(_ post: Post) async throws
     func delete(_ post: Post) async throws
     func favorite(_ post: Post) async throws
@@ -25,7 +26,11 @@ protocol PostsRepositoryProtocol {
 struct PostsRepositoryStub: PostsRepositoryProtocol {
     let state: Loadable<[Post]>
     
-    func fetchPosts() async throws -> [Post] {
+    func fetchAllPosts() async throws -> [Post] {
+        return try await state.simulate()
+    }
+    
+    func fetchFavoritePosts() async throws -> [Post] {
         return try await state.simulate()
     }
     
@@ -41,8 +46,16 @@ struct PostsRepositoryStub: PostsRepositoryProtocol {
 struct PostsRepository: PostsRepositoryProtocol {
     let postsReference = Firestore.firestore().collection("posts_v1")
     
-    func fetchPosts() async throws -> [Post] {
-        let snapshot = try await postsReference
+    func fetchAllPosts() async throws -> [Post] {
+        return try await fetchPosts(from: postsReference)
+    }
+    
+    func fetchFavoritePosts() async throws -> [Post] {
+        return try await fetchPosts(from: postsReference.whereField("isFavorite", isEqualTo: true))
+    }
+    
+    private func fetchPosts(from query: Query) async throws -> [Post] {
+        let snapshot = try await query
             .order(by: "timestamp", descending: true)
             .getDocuments()
         return snapshot.documents.compactMap { document in
